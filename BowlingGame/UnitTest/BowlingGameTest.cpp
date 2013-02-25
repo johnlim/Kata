9f970219e75c8ac65fe0e4095a4ccb233dccc85e
John Lim Ding Wei <johnlim.dingwei@gmail.com>
2013-02-25 20:16:01 +0800

    Expected error: Expected 16 but was 13.

--- BowlingGame/UnitTest/BowlingGameTest.cpp
+++ BowlingGame/UnitTest/BowlingGameTest.cpp
@@ -26,3 +26,12 @@ TEST(BowlingGame, testAllOnes)
   rollMany(20, 1);
   LONGS_EQUAL(20, g.score());
 }
+
+TEST(BowlingGame, testOneSpare)
+{
+  g.roll(5);
+  g.roll(5); //spare
+  g.roll(3);
+  rollMany(17,0);
+  LONGS_EQUAL(16, g.score());
+}
