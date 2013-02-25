6c34c9ca83b8408ea09db3b9f93e0368e8034e23
John Lim Ding Wei <johnlim.dingwei@gmail.com>
2013-02-25 19:57:42 +0800

    Removed duplication of game creation.

--- BowlingGame/UnitTest/BowlingGameTest.cpp
+++ BowlingGame/UnitTest/BowlingGameTest.cpp
@@ -3,29 +3,25 @@
 
 TEST_GROUP(BowlingGame)
 {
+  Game g;
 };
 
 TEST(BowlingGame, testGutterGame)
 {
-   Game* g = new Game;
-   
    for(int i=0; i<20; i++)
    {
-     g->roll(0);
+     g.roll(0);
    }
 
-   LONGS_EQUAL(0, g->score());
-
-   delete g;
+   LONGS_EQUAL(0, g.score());
 }
 
 TEST(BowlingGame, testAllOnes)
 {
-  Game* g = new Game;
   for(int i=0; i<20; i++)
   {
-    g->roll(1);
+    g.roll(1);
   }
 
-  LONGS_EQUAL(20, g->score());
+  LONGS_EQUAL(20, g.score());
 }
