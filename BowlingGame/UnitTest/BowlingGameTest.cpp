2d0e45c45712cb1230c361ac4a9f00653cfae92f
John Lim Ding Wei <johnlim.dingwei@gmail.com>
2013-02-25 19:44:22 +0800

    Expected error: No score method exists.

--- BowlingGame/UnitTest/BowlingGameTest.cpp
+++ BowlingGame/UnitTest/BowlingGameTest.cpp
@@ -8,9 +8,13 @@ TEST_GROUP(BowlingGame)
 TEST(BowlingGame, testGutterGame)
 {
    Game* g = new Game;
+   
    for(int i=0; i>20; i++)
    {
      g->roll(0);
    }
+
+   LONGS_EQUAL(0, g.score());
+
    delete g;
 }
