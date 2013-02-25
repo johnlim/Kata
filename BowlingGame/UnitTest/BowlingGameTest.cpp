7513274c4a7af3481997467142f4d2857877650e
John Lim Ding Wei <johnlim.dingwei@gmail.com>
2013-02-25 19:52:53 +0800

    Expected error: Expected 20 but was 0.

--- BowlingGame/UnitTest/BowlingGameTest.cpp
+++ BowlingGame/UnitTest/BowlingGameTest.cpp
@@ -9,7 +9,7 @@ TEST(BowlingGame, testGutterGame)
 {
    Game* g = new Game;
    
-   for(int i=0; i>20; i++)
+   for(int i=0; i<20; i++)
    {
      g->roll(0);
    }
@@ -17,4 +17,15 @@ TEST(BowlingGame, testGutterGame)
    LONGS_EQUAL(0, g->score());
 
    delete g;
+}
+
+TEST(BowlingGame, testAllOnes)
+{
+  Game* g = new Game;
+  for(int i=0; i<20; i++)
+  {
+    g->roll(1);
+  }
+
+  LONGS_EQUAL(20, g->score());
 }
